a11da6a448fb18bfa901462aad5f98f3bcd39786
Your Name <you@example.com>
2019-02-02 01:56:06 +0600

    Add_Ignore

---
+++ CoffeShop/CoffeShop/CoffeeShopUi.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CoffeShop
+{
+    partial class CoffeeShopUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.purchaseInfoRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.customerNameTextBox = new System.Windows.Forms.TextBox();
+            this.contactNumberTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.orderComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Contact No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Order";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(106, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Quantity";
+            // 
+            // purchaseInfoRichTextBox
+            // 
+            this.purchaseInfoRichTextBox.Location = new System.Drawing.Point(520, 37);
+            this.purchaseInfoRichTextBox.Name = "purchaseInfoRichTextBox";
+            this.purchaseInfoRichTextBox.Size = new System.Drawing.Size(251, 352);
+            this.purchaseInfoRichTextBox.TabIndex = 2;
+            this.purchaseInfoRichTextBox.Text = "";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(266, 322);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // customerNameTextBox
+            // 
+            this.customerNameTextBox.Location = new System.Drawing.Point(266, 85);
+            this.customerNameTextBox.Name = "customerNameTextBox";
+            this.customerNameTextBox.Size = new System.Drawing.Size(200, 20);
+            this.customerNameTextBox.TabIndex = 4;
+            // 
+            // contactNumberTextBox
+            // 
+            this.contactNumberTextBox.Location = new System.Drawing.Point(266, 121);
+            this.contactNumberTextBox.Name = "contactNumberTextBox";
+            this.contactNumberTextBox.Size = new System.Drawing.Size(200, 20);
+            this.contactNumberTextBox.TabIndex = 4;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(266, 162);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(200, 20);
+            this.addressTextBox.TabIndex = 4;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(266, 259);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(200, 20);
+            this.quantityTextBox.TabIndex = 4;
+            // 
+            // orderComboBox
+            // 
+            this.orderComboBox.FormattingEnabled = true;
+            this.orderComboBox.Items.AddRange(new object[] {
+            "Black",
+            "Cold",
+            "Hot",
+            "Regular"});
+            this.orderComboBox.Location = new System.Drawing.Point(266, 225);
+            this.orderComboBox.Name = "orderComboBox";
+            this.orderComboBox.Size = new System.Drawing.Size(200, 21);
+            this.orderComboBox.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(83, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(421, 352);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customer Information";
+            // 
+            // CoffeeShopUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.orderComboBox);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.contactNumberTextBox);
+            this.Controls.Add(this.customerNameTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.purchaseInfoRichTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CoffeeShopUi";
+            this.Text = "Coffee Shop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox purchaseInfoRichTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox customerNameTextBox;
+        private System.Windows.Forms.TextBox contactNumberTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.ComboBox orderComboBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ CoffeShop/CoffeShop/CoffeeShopUi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CoffeShop
+{
+    public partial class CoffeeShopUi : Form
+    {
+        public CoffeeShopUi()
+        {
+            InitializeComponent();
+        }
+        int totalPrice;
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            purchaseInfoRichTextBox.Text = "****  Purchase Information  ****" + "\n\n"
+                + "Name: " + customerNameTextBox.Text + "\n" + "Contact No: " + contactNumberTextBox.Text
+                + "\n" + "Address: " + addressTextBox.Text + "\n" + "Order: " + orderComboBox.Text + "\n"
+                + "Quantity: " + quantityTextBox.Text + "\n\n" + "**** Total Price ****" + "\n";
+            int quantity = Convert.ToInt32(quantityTextBox.Text);
+            if (orderComboBox.Text == "")
+            {
+                MessageBox.Show("Please Select an Order!");
+            }
+            else if (orderComboBox.Text == "Black")
+            {
+                // Pice of Black Coffee is 120 BDT
+                totalPrice = quantity*120;
+            }
+            else if (orderComboBox.Text == "Cold")
+            {
+                // Pice of Cold Coffee is 100 BDT
+                totalPrice = quantity * 100;
+            }
+            else if (orderComboBox.Text == "Hot")
+            {
+                // Pice of Hot Coffee is 90 BDT
+                totalPrice = quantity * 90;
+            }
+            else if (orderComboBox.Text == "Regular")
+            {
+                // Pice of Regular Coffee is 80 BDT
+                totalPrice = quantity * 80;
+            }
+            purchaseInfoRichTextBox.Text += totalPrice;
+        }
+    }
+}
